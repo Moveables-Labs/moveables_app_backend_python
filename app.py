ea070423bf6dc7f9d6a89e46602034ba4c1a390f
Ohiorenua <50307290+Ohior@users.noreply.github.com>
2023-10-03 19:08:24 +0100

    set easy run

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify, make_response
+from flask_restful import Resource, Api
+# interact with db
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({"greetings" : "Hello world"})
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
